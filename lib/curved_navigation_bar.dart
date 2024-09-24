86d30fad87174e3ed2bda7fafe174e7715e2d229
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2024-09-24 16:53:58 +0300

    remove height assert

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -41,7 +41,7 @@ class CurvedNavigationBar extends StatefulWidget {
   })  : letIndexChange = letIndexChange ?? ((_) => true),
         assert(items.length >= 1),
         assert(0 <= index && index < items.length),
-        assert(0 <= height && height <= defaultHeight!),
+        // assert(0 <= height && height <= defaultHeight!),
         super(key: key);
 
   @override
